41282aea732c0e53908a919f54e78828f6e39d37
Velpre <veljko_kv@hotmail.com>
2021-11-15 11:55:14 +0100

    header & navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from "react";
+import Header from "./components/Header.js";
 
 function App() {
   return (
-    <h1>Starting project</h1>
-      );
+    <div>
+      <Header></Header>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <h1>Tester</h1>
+      <img src={require("../images/header-image.jpg")} alt="" />
+    </div>
+  );
+}
