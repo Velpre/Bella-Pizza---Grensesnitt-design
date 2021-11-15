520fd3cd7cde7d42f81f7f19ba0d3e7490d4d203
Daniel Lysak <Daniel_kn@live.no>
2021-11-15 16:50:17 +0100

    Added navbar and hero section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import CssBaseline from '@mui/material/CssBaseline';
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
+
 
 function App() {
   return (
     <React.Fragment>
       <CssBaseline>
         <Navbar />
+        <Hero />
       </CssBaseline>
     </React.Fragment>
   );
---
+++ src/components/Hero.jsx
@@ -0,0 +1,40 @@
+
+import React from "react";
+import {
+    Typography,
+    Container,
+    Button
+} from "@material-ui/core";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import headerImage from "../images/header-image.jpg";
+
+
+
+// LOCAL-STYLING
+const useStyles = makeStyles((theme) => ({
+    root: {
+        backgroundImage: `url(${headerImage})`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        height: "100vh",
+        witdh: "100%",
+        color: "red"
+    },
+    size: {
+        postion: "relative",
+        top: "100px" //change
+    }
+}));
+
+export default function Hero() {
+    const classes = useStyles();
+
+    return (
+        <div maxWidth="1">
+            <div className={classes.root}>
+                
+            </div>
+        </div>
+    );
+}
