7d0374b8a64de7bbe0e4b614a17c8367a5c018ae
Elsa Lossius <elsalos@icloud.com>
2021-11-19 14:34:05 +0100

    working

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,18 @@
-
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import {
+    Typography,
+    Container,
+    Button, 
+    ButtonGroup,
+    Box
+} from "@material-ui/core";
+import { makeStyles} from "@material-ui/core/styles";
 import headerImage from "../images/hero-image.jpeg";
-
-
+import '../css/Hero.css'
 
 // LOCAL-STYLING
 const useStyles = makeStyles((theme) => ({
-    image: {
+    root: {
         backgroundImage: `url(${headerImage})`,
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
@@ -24,8 +29,20 @@ export default function Hero() {
     const classes = useStyles();
 
     return (
-        <div maxWidth="1">
-            <div className={classes.image}>
+        <div>
+            <div className={classes.root}>
+                <ButtonGroup style={{height: "100vh", display: "flex", justifyContent: "center"}}>
+                <Box m={2} pt={3} style={{alignItems: "center"}}>
+                    <Button variant="contained" color="default">
+                        Reserver bord
+                    </Button>
+                </Box>
+                <Box m={2} pt={3} style={{justifyContent: "center"}}>
+                    <Button variant="contained" color="default">
+                        Bestill takeaway
+                    </Button>
+                </Box>
+                </ButtonGroup>
             </div>
         </div>
     );
