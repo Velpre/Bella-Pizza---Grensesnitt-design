38cf4d69e9682101c08d87f7dfa0188f1069750a
Velpre <veljko_kv@hotmail.com>
2021-11-20 01:44:28 +0100

    Finish with grid layout for contact us

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -2,80 +2,85 @@ import React from "react";
 import Footer from "./Footer";
 import BookTable from "./BookTable";
 import { Grid } from "@mui/material";
-import "../style-first-page.css";
+import "../css/ContactUs.css";
 
 export default function ContactUs(props) {
   return (
-    <Grid container className="contact-section">
-      <Grid container item xs={12} justifyContent="center">
-        <Grid item>
-          <h1>Kontakt oss</h1>
+    <div>
+      <Grid container className="contact-section">
+        <Grid container item xs={12} justifyContent="center">
+          <Grid item>
+            <h1>KONTAKT</h1>
+          </Grid>
         </Grid>
-      </Grid>
 
-      <Grid
-        item
-        spacing={4}
-        xs={12}
-        sm={6}
-        container
-        direction="column"
-        alignItems="center"
-      >
-        <Grid item>
-          <h3>E-mail</h3>
-          <a href="mailto:post@bellapizza.com">post@bellapizza.no</a>
+        <Grid
+          item
+          container
+          xs={12}
+          sm={6}
+          direction="column"
+          justifyContent="space-evenly"
+        >
+          <Grid item>
+            <h3>ADRESSE</h3>
+            <p>Romaveien 12</p>
+            <p>0368 Oslo</p>
+          </Grid>
+          <Grid item>
+            <h3>TELEFON</h3>
+            <p>21 21 21 00</p>
+          </Grid>
+          <Grid item>
+            <h3>E-MAIL</h3>
+            <a href="mailto:post@bellapizza.com">post@bellapizza.no</a>
+          </Grid>
+          <Grid item>
+            <BookTable></BookTable>
+          </Grid>
         </Grid>
-        <Grid item>
-          <h3>Telefon</h3>
-          <p>21 21 21 00</p>
-        </Grid>
-        <Grid item>
-          <h3>Adresse</h3>
-          <p>Romaveien 12</p>
-          <p>0368 Oslo</p>
-        </Grid>
-        <Grid item>
-          <BookTable></BookTable>
-        </Grid>
-      </Grid>
 
-      <Grid
-        item
-        spacing={8}
-        xs={12}
-        sm={6}
-        container
-        direction="column"
-        alignItems="center"
-      >
-        <Grid item>
-          <h3>Åpningstider</h3>
-          <p>Mandag-Torsdag</p>
-          <p>11:00 - 00:00</p>
-          <p>Lørdag-Søndag</p>
-          <p>16:00 - 00:00</p>
+        <Grid
+          container
+          item
+          xs={12}
+          sm={6}
+          direction="column"
+          justifyContent="space-evenly"
+        >
+          <Grid item>
+            <h3>ÅPNINGSTIDER</h3>
+            <p>Mandag-Torsdag</p>
+            <p>11:00 - 00:00</p>
+            <p>Lørdag-Søndag</p>
+            <p>16:00 - 00:00</p>
+          </Grid>
+          <Grid item>
+            <iframe
+              className="map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d16003.204024194303!2d10.739215484427733!3d59.90890028754498!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46416e8a1dd5d001%3A0xcdf1706e3a58f28c!2sBella%20Bambina%20Ristorante!5e0!3m2!1sno!2sno!4v1637335043493!5m2!1sno!2sno"
+              width="300"
+              height="200"
+              allowfullscreen=""
+              loading="lazy"
+            ></iframe>
+          </Grid>
         </Grid>
-        <Grid item>
-          <p>Her kommer kart</p>
-          <p>Her kommer kart</p>
-          <p>Her kommer kart</p>
-          <p>Her kommer kart</p>
-        </Grid>
-      </Grid>
 
-      <Grid
-        container
-        item
-        xs={12}
-        alignItems="flex-end"
-        justifyContent="center"
-      >
-        <Grid item>
-          <Footer></Footer>
-        </Grid>
+        <Footer></Footer>
+
+        {/*         <Grid
+          container
+          item
+          xs={12}
+          justifyContent="center"
+          
+        >
+          <Grid item>
+            <Footer></Footer>
+          </Grid>
+        </Grid> */}
       </Grid>
-    </Grid>
+    </div>
   );
 }
-
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import { Email, Facebook, Instagram } from "@mui/icons-material";
 import { Grid } from "@mui/material";
+import "../css/ContactUs.css";
 
 export default function Footer() {
   function test() {
     alert();
   }
   return (
-    <div>
-      <Email fontSize="large"></Email>
-      <Facebook fontSize="large"></Facebook>
-      <Instagram fontSize="large"></Instagram>
-    </div>
+    <Grid
+      container
+      item
+      className="footer-container"
+      justifyContent="center"
+      alignItems="flex-end"
+      spacing={15}
+    >
+      <Grid item>
+        <Email fontSize="large"></Email>
+      </Grid>
+      <Grid item>
+        <Facebook fontSize="large"></Facebook>
+      </Grid>
+      <Grid item>
+        <Instagram fontSize="large"></Instagram>
+      </Grid>
+    </Grid>
   );
 }
