87ff9962ca6e3e88cbaa81339da32e22a2a8a652
Velpre <veljko_kv@hotmail.com>
2021-11-20 14:55:43 +0100

    Finish with about us layout

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -12,33 +12,27 @@ import ArrowIcon from "@mui/icons-material/ArrowForwardIos";
 
 export default function AboutUs() {
   return (
-    <Grid className="about-us-container" container>
-      <Grid container xs={6} item>
-        <Grid item>
-          <h1>ITALIENSK I ÅNDEN</h1>
-          <Typography className="about-us-txt">
-            Vår kjærlighetsaffære med italiensk mat er langsiktig. I 1965 visste
-            en klok mann kalt Marco Rossi at pizza i Oslo kunne være bedre, og
-            åpnet Pizzeria Bella i hjertet av byen. Du finner mange
-            valgmuligheter, inkludert klassisk italiensk mat sammen med våre
-            egne signaturretter.
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Button
-            id="btn-reservation"
-            variant="contained"
-            endIcon={<ArrowIcon />}
-          >
-            Bestill bord
-          </Button>
-        </Grid>
+    <Grid className="about-us-container" container spacing={5}>
+      <Grid item xs={9} md={4}>
+        <h1>ITALIENSK I ÅNDEN</h1>
+        <Typography className="about-us-txt">
+          Vår kjærlighetsaffære med italiensk mat er langsiktig. I 1965 visste
+          en klok mann kalt Marco Rossi at pizza i Oslo kunne være bedre, og
+          åpnet Pizzeria Bella i hjertet av byen. Du finner mange
+          valgmuligheter, inkludert klassisk italiensk mat sammen med våre egne
+          signaturretter.
+        </Typography>
+        <Button
+          id="btn-reservation"
+          variant="contained"
+          endIcon={<ArrowIcon />}
+        >
+          Bestill bord
+        </Button>
       </Grid>
 
-      <Grid container item xs={6}>
-        <Grid item className="about-us-image">
-          <img id="restaurant-img" src={RestaurantPhoto} />
-        </Grid>
+      <Grid item xs={12} md={6} className="about-us-image">
+        <img id="restaurant-img" src={RestaurantPhoto} />
       </Grid>
     </Grid>
   );
