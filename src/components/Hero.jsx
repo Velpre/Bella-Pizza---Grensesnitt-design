85c1f60c863c9f2fb566064e566f07116909e274
Velpre <veljko_kv@hotmail.com>
2021-11-21 14:15:31 +0100

    position of hero btns(need media query)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,59 +1,59 @@
 import React from "react";
 import {
-    Typography,
-    Container,
-    Button,
-    ButtonGroup,
-    Box,
-    useMediaQuery,
-    Grid,
+  Typography,
+  Container,
+  Button,
+  ButtonGroup,
+  Box,
+  useMediaQuery,
+  Grid,
+  FormHelperText,
 } from "@material-ui/core";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import headerImage from "../images/hero-image.jpeg";
-import '../css/Hero.css'
-
+import "../css/Hero.css";
 
 // LOCAL-STYLING
 const useStyles = makeStyles((theme) => ({
-    root: {
-        backgroundImage: `url(${headerImage})`,
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        height: "100vh",
-        position: "relative",
-    },
+  root: {
+    backgroundImage: `url(${headerImage})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    height: "100vh",
+  },
+
+  btnDiv: {
+    position: "absolute",
+    bottom: "10%",
+    left: "50%",
+    transform: "translateX(-50%)",
+    display: "flex",
+    alignItems: "flex-end",
+    justifyContent: "space-around",
+    width: "50%",
+  },
 }));
 
 export default function Hero() {
-    const classes = useStyles();
-    const theme = useTheme();
-    const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const classes = useStyles();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
-
-    return (
-        <div id="hero-div" className={classes.root}>
-            <Grid container id="hero-container"
-                spacing={5}
-                direction={`${isMobile ? "column" : "row"}`}
-                alignItems="center"
-                justifyContent="center"
-                style={{ height: '100vh', width: '100%', position: 'relative', top: "10%" }}>
-                <Grid item >
-                    <Button className="hero-btn" variant="contained" color="primary" size="large" >
-                        Bestill takeaway
-                    </Button>
-                </Grid>
-                <Grid item>
-                    <Button className="hero-btn" variant="contained"
-                        size="large"
-                        color="primary"
-                        href="/meny" >
-
-                        Reserver bord
-                    </Button>
-                </Grid>
-            </Grid>
-        </div >
-    );
+  return (
+    <div className={classes.root}>
+      <div className={classes.btnDiv}>
+        <div className="hero-btn">
+          <Button variant="contained" color="primary" size="large">
+            TAKEAWAY
+          </Button>
+        </div>
+        <div className="hero-btn">
+          <Button variant="contained" size="large" color="primary" href="/meny">
+            BESTILL BORD
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
