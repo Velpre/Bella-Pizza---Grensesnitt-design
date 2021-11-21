90e10055e28de1d22b2775ff96efbe83f9c722ba
Velpre <veljko_kv@hotmail.com>
2021-11-21 21:02:08 +0100

    commit before changing booktabel modal

--- src/pages/Meny.jsx
+++ src/pages/Meny.jsx
@@ -1,23 +1,19 @@
 import React from "react";
 
 import Navbar from "../components/Navbar";
-import { UserContext } from "../App";
-import { useContext } from "react";
-import DisplayMenu from "../components/DisplayMenu"
-
-
-
+import DisplayMenu from "../components/DisplayMenu";
+import Footer from "../components/Footer";
 
 function Meny() {
-
-    return (
-        <>
-            <div style={{ backgroundColor: "#c0c0c01a" }}>
-                <Navbar />
-                <DisplayMenu />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div style={{ backgroundColor: "#c0c0c01a" }}>
+        <Navbar />
+        <DisplayMenu />
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default Meny;
